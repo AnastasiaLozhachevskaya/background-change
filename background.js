144b6fce2c0658d5d8f74daa8d54dca3f380e2ac
Anastasiya Lozhachevskaya <stesha1990@mail.ru>
2016-12-01 18:23:51 +0200

    file added

---
+++ background.js
@@ -0,0 +1,21 @@
+var pageBg = document.querySelector('body > form');
+var className = ['f-bg-ultra-green', 'f-bg-ultra-yellow', 'f-bg-ultra-coral', 'f-bg-ultra-lilac', 'f-bg-ultra-blue'];
+var prevScreen = 0;
+var originalClass = pageBg.className;
+pageBg.classList.add('f-bg-ultra-blue');
+
+
+window.onscroll = function () {
+	var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	var screen = Math.round(window.pageYOffset / height);
+
+	if (!(screen == prevScreen)) {
+
+		var firstClass = className.shift();
+		className.push(firstClass);
+
+		pageBg.className = originalClass + ' ' + firstClass;
+		prevScreen = screen;
+	}
+	
+}
